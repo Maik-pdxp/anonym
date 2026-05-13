ffffcd6b3c3f2be925c2d37e89048be6d5ea1569
Maik-pdxp <maik.patzwald@gmx.de>
2026-05-13 16:02:41 +0200

    fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import ImpressumScreen from './components/ImpressumScreen.jsx'
+import DatenschutzScreen from './components/DatenschutzScreen.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/impressum" element={<ImpressumScreen />} />
+        <Route path="/datenschutz" element={<DatenschutzScreen />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
